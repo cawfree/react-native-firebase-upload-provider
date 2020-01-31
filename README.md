ab0d40ec4b1e3e97ebfeb1d1d29e0beed06d7756
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2020-01-31 15:30:44 +0000

    Finalized frontend synchronization and retry method.

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # react-native-firebase-upload-provider
-📸Easily and quickly upload rich media to Firebase Storage.
+📸Easily and quickly upload rich media to Firebase Storage. This library safely handles all of the lower level firebase storage transactions, whilst providing a sensible interface to synchronize your frontend with the transaction state.
+
+## Getting Started 
+
+Using [`npm`]():
+
+```bash
+npm install --save react-native-firebase-upload-provider
+```
+
+Using [`yarn`]():
+
+```bash
+yarn add react-native-firebase-upload-provider
+```
+
+## Requirements
+
+  - Make sure you've added the `google-services.json` and `GoogleService-Info.plist` to your `*/android/app/` and `*/ios/` directories respectively.
+    - If this sounds new to you, it would be worth checking out the [Getting Started](https://rnfirebase.io/docs/v5.x.x/getting-started) tutorials on [react-native-firebase](https://rnfirebase.io/).
+  - Once your project is hooked up, head over to your project in [Firebase](https://firebase.google.com/) and make sure you've [enabled Firebase Storage](https://firebase.google.com/docs/storage/web/start).
+  - Finally, you'll need to make sure your application has the appropriate permissions to write to the storage bucket.
+    - By default, they do not permit anything to be written. For testing purposes, you can go ahead and turn `false` into `true` to permit anyone to read and write.
+    - **Note**: This is not a safe configuration to use within a production environment, and you **will** regret it.
+
+```
+rules_version = '2';
+service firebase.storage {
+  match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read, write: if false;
+    }
+  }
+}
+
+```
+
+## Usage
+
+1. First, wrap your application with the `FirebaseUploadProvider`:
+
+```javascript
+import React from 'react';
+import { Text } from 'react-native-firebase';
+import FirebaseUploadProvider from 'react-native-firebase-upload-provider';
+
+export default () => (
+  <FirebaseUploadProvider
+    supportedMimeTypes={[
+      "image/png",
+      "image/jpeg"
+    ]}
+  >
+    <Text
+      children="Best app ever."
+    />
+  </FirebaseUploadProvider>
+);
+```
+
+This provides all of the data dependencies required to perform a file upload from basically anywhere in your application. In particular, notice that we're required to specify which [Mime Types](https://www.npmjs.com/package/mime-types) are permitted to be uploaded to your [Storage Bucket](https://cloud.google.com/storage/docs/creating-buckets). By default, no mime types are specified as a safeguard to prevent any users from uploading potentially undesirable content.
+
+2. Next, there are the [hooks](https://reactjs.org/docs/hooks-intro.html). There are two you'll be interested in:
+
+```javascript
+import { useFirebaseUploads } from 'react-native-firebase-upload-provider';
+
+const UploadButton = ({ ...extraProps }) => {
+  const { useUploads, requestUpload } = useFirebaseUploads();
+  return ...;
+}
+```
+
+  2.1 `requestUpload(uri)`
+  This hook is used to upload an asset from the local filesystem up to firebase. It is a **synchronous** call, which when invoked returns an array with the following shape:
+
+```javascript
+const [uploadId, beginUpload] = requestUpload('file://path/to/some/asset.jpeg');
+```
+  
+  The `uploadId` is an internal [`uuidv4`](https://www.npmjs.com/package/uuid) which is used to uniquely track the transaction of the specified file, whilst `beginUpload` is a function which when invoked attempts to start the transaction, or restart the transaction if it had previously failed.
+
+  Upon completion, `beginUpload` resolves with the raw result of the transaction.
+
+  2.2 `useUploads()`
+  This hook allows you to interrogate the state of the ongoing transactions, and have your registered component re-rensder when any of the transactions have been updated. This is how we can determine things like the state of the task, the number of `bytesTransferred` and the `totalNumberOfBytes`, etc.
+
+```javascript
+const { useUploads } = useFirebaseUploads();
+const uploads = useUploads();
+return (
+  <Text
+    children={JSON.stringify(uploads)}
+  />
+);
+```
+
+  This allows you to easily synchronize the interface presented to your user with the ongoing transaction.
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
+  
---
+++ src/index.js
@@ -0,0 +1,184 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Map } from "immutable";
+import { useMutator } from "react-use-mutator";
+import uuidv4 from "uuid/v4";
+import { lookup } from "react-native-mime-types";
+import storage from "@react-native-firebase/storage";
+
+export const TaskStatus = {
+  CREATED: 0,
+  UPLOADING: 1,
+  FINISHED: 2,
+  ERROR: 3
+};
+
+const UploadProviderError = Object.freeze(
+  new Error(
+    "It looks like you haven't declared a FirebaseUploadProvider at the root of your application."
+  )
+);
+
+const defaultMimeTypes = Object.freeze([]);
+
+const defaultContext = Object.freeze({
+  useUploads: () => {
+    throw UploadProviderError;
+  },
+  requestUpload: () => Promise.reject(UploadProviderError)
+});
+
+const FirebaseUploadContext = React.createContext(defaultContext);
+
+export const useFirebaseUploads = () => React.useContext(FirebaseUploadContext);
+
+const createTask = (uri, mimeType) =>
+  Object.freeze({
+    status: TaskStatus.CREATED,
+    uri,
+    mimeType,
+    bytesTransferred: 0,
+    totalBytes: undefined
+  });
+
+const updateTaskStatus = (task, status) =>
+  Object.freeze({
+    ...task,
+    status
+  });
+
+const updateTaskProgress = (task, bytesTransferred, totalBytes) =>
+  Object.freeze({
+    ...task,
+    bytesTransferred,
+    totalBytes
+  });
+
+const publishTaskUpdates = (uploadTask, mutate, uploadId) => {
+  const unsubscribe = uploadTask.on(
+    storage.TaskEvent.STATE_CHANGED,
+    snapshot => {
+      const { state, bytesTransferred, totalBytes } = snapshot;
+      mutate(state =>
+        state.set(
+          uploadId,
+          updateTaskProgress(state.get(uploadId), bytesTransferred, totalBytes)
+        )
+      );
+      if (state === storage.TaskState.SUCCESS) {
+        unsubscribe();
+      }
+    },
+    err => unsubscribe()
+  );
+};
+
+const shouldUpload = (uploadId, mutate, createRef) =>
+  Promise.resolve()
+    .then(() => {
+      const currentState = mutate();
+      const taskState = currentState.get(uploadId);
+      if (!!taskState && typeof taskState === "object") {
+        const { status } = taskState;
+        if (status === TaskStatus.CREATED || status === TaskStatus.ERROR) {
+          const nextTaskState = updateTaskStatus(
+            taskState,
+            TaskStatus.UPLOADING
+          );
+          mutate(state => state.set(uploadId, nextTaskState));
+          return Promise.resolve(nextTaskState);
+        }
+        return Promise.reject(
+          new Error("This task is not in the correct state to be uploaded.")
+        );
+      }
+
+      return Promise.reject(
+        new Error(`Attempted to upload an unrecogized uploadId, "${uploadId}".`)
+      );
+    })
+    .then(taskState => {
+      const { uri, mimeType: contentType } = taskState;
+      const ref = createRef(uploadId, taskState);
+      const uploadTask = ref.putFile(uri, { contentType });
+      // XXX: Asynchronously update state.
+      publishTaskUpdates(uploadTask, mutate, uploadId);
+      return uploadTask;
+    })
+    .then(uploadResult => {
+      mutate(state =>
+        state.set(
+          uploadId,
+          updateTaskStatus(state.get(uploadId), TaskStatus.FINISHED)
+        )
+      );
+      return Promise.resolve(uploadResult);
+    })
+    .catch(e => {
+      mutate(state =>
+        state.set(
+          uploadId,
+          updateTaskStatus(state.get(uploadId), TaskStatus.ERROR)
+        )
+      );
+      return Promise.reject(e);
+    });
+
+const requestUploadThunk = (mutate, supportedMimeTypes, createRef) => (
+  uri,
+  customCreateRef = null
+) => {
+  const mimeType = lookup(uri);
+  // TODO: Need to validate the uri and mimeType
+  if (typeof uri !== "string" || uri.length === 0) {
+    throw new Error(`Expected valid uri, encountered ${uri}.`);
+  } else if (typeof mimeType !== "string" || mimeType.length === 0) {
+    throw new Error(`Expected valid mimeType, encountered ${mimeType}.`);
+  } else if (supportedMimeTypes.indexOf(mimeType) < 0) {
+    throw new Error(
+      `Mime Type ${mimeType} is not supported. You can add this to the supportedMimeTypes prop of your provider if you wish to support kind of content.`
+    );
+  }
+  const uploadId = uuidv4();
+  mutate(state => state.set(uploadId, createTask(uri, mimeType)));
+  return [
+    uploadId,
+    () => shouldUpload(uploadId, mutate, customCreateRef || createRef)
+  ];
+};
+
+const FirebaseUploadProvider = ({
+  supportedMimeTypes,
+  createRef,
+  ...extraProps
+}) => {
+  const [useUploads, mutate] = useMutator(() => Map());
+  const [requestUpload] = useState(() =>
+    requestUploadThunk(mutate, supportedMimeTypes, createRef)
+  );
+  return (
+    <FirebaseUploadContext.Provider
+      value={{
+        requestUpload,
+        useUploads
+      }}
+    >
+      <React.Fragment {...extraProps} />
+    </FirebaseUploadContext.Provider>
+  );
+};
+
+FirebaseUploadProvider.propTypes = {
+  supportedMimeTypes: PropTypes.arrayOf(PropTypes.string),
+  createRef: PropTypes.func
+};
+
+FirebaseUploadProvider.defaultProps = {
+  supportedMimeTypes: defaultMimeTypes,
+  createRef: (taskId, { mimeType }) =>
+    storage()
+      .ref(mimeType.substring(0, mimeType.indexOf("/")))
+      .child(taskId)
+};
+
+export default FirebaseUploadProvider;
